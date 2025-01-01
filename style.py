62e50b3edac15349c707863dcaaefef7a5871269
Hakari Bibani <hakary.jalal33@gmail.com>
2025-01-01 20:59:03 +0300

    Update style.py

--- style.py
+++ style.py
@@ -1,11 +1,18 @@
+# style.py
 import streamlit as st
 
 def apply_styles():
-    st.markdown(
-        """
+    st.markdown("""
         <style>
-        .main { background-color: #f0f0f0; }
+        .big-font {
+            font-size:50px !important;
+            color: red;
+            animation: blink 1s infinite;
+        }
+        @keyframes blink {
+            0% { opacity: 1; }
+            50% { opacity: 0; }
+            100% { opacity: 1; }
+        }
         </style>
-        """,
-        unsafe_allow_html=True
-    )
+        """, unsafe_allow_html=True)
